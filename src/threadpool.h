865bd77ca12ed2ca0652d4b940119564780115d3
Ch4120N <Ch4120N@Proton.me>
2026-06-21 00:14:13 +0430

    added public structure of the ThreadPool class

--- src/threadpool.h
+++ src/threadpool.h
@@ -20,4 +20,45 @@ private:
     std::mutex                        mutex_;
     std::condition_variable           cv_;
     bool                              stopFlag_;
+public:
+    explicit ThreadPool(size_t numThreads) : stopFlag_(false) {
+        for (size_t i = 0; i < numThreads; ++i) {
+            workers_.emplace_back([this] {
+                while (true) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(mutex_);
+                        cv_.wait(lock, [this] { return stopFlag_ || !tasks_.empty(); });
+                        if (stopFlag_ && tasks_.empty()) return;
+                        task = std::move(tasks_.front());
+                        tasks_.pop();
+                    }
+                    task();
+                }
+            });
+        }
+    }
+
+    template <typename F>
+    std::future<void> enqueue(F&& f) {
+        auto task = std::make_shared<std::packaged_task<void()>>(std::forward<F>(f));
+        std::future<void> result = task->get_future();
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            tasks_.emplace([task]() { (*task)(); });
+        }
+        cv_.notify_one();
+        return result;
+    }
+
+    ~ThreadPool() {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            stopFlag_ = true;
+        }
+        cv_.notify_all();
+        for (auto& w : workers_) {
+            if (w.joinable()) w.join();
+        }
+    }
 };
