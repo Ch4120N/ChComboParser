633c9ea169a2fe11ea655510613a34b578b46dd6
Ch4120N <Ch4120N@Proton.me>
2026-06-21 00:16:42 +0430

    added some utility functions

--- src/parser.h
+++ src/parser.h
@@ -25,3 +25,18 @@ struct ParseStats {
     std::atomic<size_t> duplicatesRemoved{0};
 };
 
+inline std::string trim(const std::string& s) {
+    size_t start = s.find_first_not_of(" \t\r\n");
+    if (start == std::string::npos) return "";
+    size_t end = s.find_last_not_of(" \t\r\n");
+    return s.substr(start, end - start + 1);
+}
+
+inline void toLowerInPlace(std::string& s) {
+    for (auto& c : s) c = static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
+}
+
+inline void toUpperInPlace(std::string& s) {
+    for (auto& c : s) c = static_cast<char>(std::toupper(static_cast<unsigned char>(c)));
+}
+
