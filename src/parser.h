bbf62f000b32af6527096b5031d66017986b4e85
Ch4120N <Ch4120N@Proton.me>
2026-06-21 00:16:08 +0430

    added an lightweight parser stats

--- src/parser.h
+++ src/parser.h
@@ -15,3 +15,13 @@
 #include <cstring>
 #include <chrono>
 
+struct ParseStats {
+    std::atomic<size_t> totalLines{0};
+    std::atomic<size_t> validExtracted{0};
+    std::atomic<size_t> skippedEmpty{0};
+    std::atomic<size_t> skippedLength{0};
+    std::atomic<size_t> skippedNoSymbol{0};
+    std::atomic<size_t> skippedIndexOOB{0};
+    std::atomic<size_t> duplicatesRemoved{0};
+};
+
